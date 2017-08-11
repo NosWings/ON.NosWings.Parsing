ffa005e23afdf20f1c7c6f500b1ee37f6e203d4b
Blowa <blowaxd693@gmail.com>
2017-08-12 00:29:46 +0200

    [UPDATE] Parsing with Raids

---
+++ randomboxes/cuby.sql
@@ -0,0 +1,17 @@
+INSERT INTO [OpenNos].[dbo].[RollGeneratedItem] (
+	[OriginalItemDesign],
+	[OriginalItemVNum],
+	[Probability],
+	[ItemGeneratedAmount],
+	[ItemGeneratedVNum],
+	[IsRareRandom],
+	[MinimumOriginalItemRare],
+	[MaximumOriginalItemRare],
+	[ItemGeneratedUpgrade]
+)
+VALUES
+	(0, 440, 5, 1, 440, 0, 0, 0,0),
+	(0, 440, 5, 1, 440, 0, 0, 0,0),
+	(0, 440, 5, 1, 440, 0, 0, 0,0),
+	(0, 440, 5, 1, 440, 0, 0, 0,0),
+	(0, 440, 5, 1, 440, 0, 0, 0,0);
