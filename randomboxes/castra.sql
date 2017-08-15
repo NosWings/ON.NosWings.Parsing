c0f6d5b17f76aca26dbb797d8ac7a688f5b38dd0
Arthur <arthur.despeaux@epitech.eu>
2017-08-15 18:58:25 +0200

    [ADD] Random Boxes + grenigas raid

---
+++ randomboxes/castra.sql
@@ -0,0 +1,21 @@
+INSERT INTO [OpenNos].[dbo].[RollGeneratedItem] (
+	[OriginalItemDesign],
+	[OriginalItemVNum],
+	[Probability],
+	[ItemGeneratedAmount],
+	[ItemGeneratedVNum],
+	[IsRareRandom],
+	[MinimumOriginalItemRare],
+	[MaximumOriginalItemRare],
+	[ItemGeneratedUpgrade]
+)
+VALUES
+	(2, 302, 15, 1, 312, 1, 0, 7,0),
+	(2, 302, 15, 1, 313, 1, 0, 7,0),
+	(2, 302, 15, 1, 314, 1, 0, 7,0),
+	(2, 302, 12, 1, 318, 1, 0, 7,0),
+	(2, 302, 12, 1, 320, 1, 0, 7,0),
+	(2, 302, 10, 1, 263, 1, 0, 7,0),
+	(2, 302, 10, 1, 266, 1, 0, 7,0),
+	(2, 302, 10, 1, 269, 1, 0, 7,0),
+	(2, 302, 1, 1, 2516, 1, 0, 7,0);
---
+++ randomboxes/jack.sql
@@ -0,0 +1,23 @@
+INSERT INTO [OpenNos].[dbo].[RollGeneratedItem] (
+	[OriginalItemDesign],
+	[OriginalItemVNum],
+	[Probability],
+	[ItemGeneratedAmount],
+	[ItemGeneratedVNum],
+	[IsRareRandom],
+	[MinimumOriginalItemRare],
+	[MaximumOriginalItemRare],
+	[ItemGeneratedUpgrade]
+)
+VALUES
+	(3, 302, 10, 1, 316, 1, 0, 7,0),
+	(3, 302, 10, 1, 319, 1, 0, 7,0),
+	(3, 302, 10, 1, 141, 1, 0, 7,0),
+	(3, 302, 10, 1, 148, 1, 0, 7,0),
+	(3, 302, 10, 1, 155, 1, 0, 7,0),
+	(3, 302, 10, 1, 292, 1, 0, 7,0),
+	(3, 302, 10, 1, 290, 1, 0, 7,0),
+	(3, 302, 10, 1, 294, 1, 0, 7,0),
+	(3, 302, 7, 1, 298, 1, 0, 7,0),
+	(3, 302, 7, 1, 296, 1, 0, 7,0),
+	(3, 302, 6, 1, 272, 1, 0, 7,0);
