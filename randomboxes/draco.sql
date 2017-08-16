f1da4636c49d0a6daf4a4e09a4856a121eec2853
Arthur <arthur.despeaux@epitech.eu>
2017-08-16 19:46:19 +0200

    [ADD] valak raid reward & few raid boxes

---
+++ randomboxes/draco.sql
@@ -0,0 +1,20 @@
+INSERT INTO [OpenNos].[dbo].[RollGeneratedItem] (
+	[OriginalItemDesign],
+	[OriginalItemVNum],
+	[Probability],
+	[ItemGeneratedAmount],
+	[ItemGeneratedVNum],
+	[IsRareRandom],
+	[MinimumOriginalItemRare],
+	[MaximumOriginalItemRare],
+	[ItemGeneratedUpgrade]
+)
+VALUES
+	(16, 302, 3, 1, 4500, 1, 0, 7,0),
+	(16, 302, 3, 1, 4501, 1, 0, 7,0),
+	(16, 302, 3, 1, 4502, 1, 0, 7,0),
+	(16, 302, 16, 1, 2519, 1, 0, 7,0),
+	(16, 302, 15, 1, 2518, 1, 0, 7,0),
+	(16, 302, 20, 5, 2282, 1, 0, 7,0),
+	(16, 302, 20, 5, 1030, 1, 0, 7,0),
+	(16, 302, 20, 3, 2349, 1, 0, 7, 0);
