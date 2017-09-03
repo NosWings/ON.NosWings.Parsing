3757061564ea566a5db93ed3dcb26b14e5dda59f
Arthur <arthur.despeaux@epitech.eu>
2017-09-03 23:44:19 +0200

    [UPDATE] Scripts

---
+++ various_db_scripts/MapNPC/STUFF_ARCHER.sql
@@ -0,0 +1,43 @@
+DECLARE @NpcId INT = 21009
+DECLARE @NpcDialog SMALLINT = 9770
+DECLARE @NpcEffect SMALLINT = 0
+DECLARE @NpcIsDisabled SMALLINT = 0
+DECLARE @NpcIsMoving SMALLINT = 0
+DECLARE @NpcIsSitting SMALLINT = 0
+
+DECLARE @NpcMapId SMALLINT = 1
+DECLARE @NpcMapX SMALLINT = 122
+DECLARE @NpcMapY SMALLINT = 80
+DECLARE @NpcPosition tinyint = 0
+
+DECLARE @NpcVnum SMALLINT = 2640
+
+DECLARE @ShopName nvarchar (255) = '[ Stuff Archer ]'
+DECLARE @ShopMenuType tinyint = 0
+DECLARE @ShopType SMALLINT = 6
+
+/*
+** THE NPC
+*/
+INSERT INTO [opennos].[dbo].[MapNpc] (
+	[MapNpcId], [Dialog], [Effect], [EffectDelay], [IsDisabled], [IsMoving], [IsSitting],
+	[MapId], [MapX], [MapY], [NpcVNum], [Position]
+)
+VALUES
+	(
+		@NpcId,	@NpcDialog, @NpcEffect, '4750', @NpcIsDisabled, @NpcIsMoving,	@NpcIsSitting,
+		@NpcMapId,	@NpcMapX,	@NpcMapY,	@NpcVnum,	@NpcPosition
+	)
+
+/*
+**	THE SHOP
+*/
+	INSERT INTO [opennos].[dbo].[Shop] (
+		[MapNpcId],
+		[MenuType], [Name], [ShopType]
+)
+VALUES
+	(
+		@NpcId,
+		@ShopMenuType, @ShopName, @ShopType
+	);
