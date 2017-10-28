fd5f56a76f6e5e7d77a4ec78ca31bc426f9c62dc
Unknown <tomtom95600@gmail.com>
2017-10-28 23:40:52 +0200

    Portal

---
+++ various_db_scripts/MAJ REBORN/Portal.sql
@@ -0,0 +1,9 @@
+INSERT INTO [opennos].[dbo].[Portal] (
+	[DestinationMapId], [DestinationX],
+	[DestinationY], [IsDisabled], [SourceMapId],
+	[SourceX],	[SourceY], [Type]
+)
+VALUES
+
+  ('243', '66', '245', '0', '228', '164', '47', '-1'),
+  ('228', '164', '47', '0', '243', '66', '245', '-1');
