ca9f687818b8bcbf07136b0cb79ab6951e5be5f9
Unknown <tpouilly24@gmail.com>
2017-10-29 13:41:00 +0100

    Change element rate and max element rate

---
+++ various_db_scripts/MAJ REBORN/Stuff/Fairys.sql
@@ -0,0 +1,94 @@
+UPDATE Item
+
+    SET MaxElementRate = 120,
+        ElementRate = 80
+        
+WHERE VNum = 4705
+
+
+UPDATE Item
+
+    SET MaxElementRate = 120,
+        ElementRate = 80
+        
+WHERE VNum = 4706
+
+
+UPDATE Item
+
+    SET MaxElementRate = 120,
+        ElementRate = 80
+        
+WHERE VNum = 4707
+
+
+UPDATE Item
+
+    SET MaxElementRate = 120,
+        ElementRate = 80
+        
+WHERE VNum = 4708
+
+
+UPDATE Item
+
+    SET MaxElementRate = 120,
+        ElementRate = 80
+        
+WHERE VNum = 4709
+
+
+UPDATE Item
+
+    SET MaxElementRate = 120,
+        ElementRate = 80
+        
+WHERE VNum = 4710
+
+
+UPDATE Item
+
+    SET MaxElementRate = 120,
+        ElementRate = 80
+        
+WHERE VNum = 4711
+
+
+UPDATE Item
+
+    SET MaxElementRate = 120,
+        ElementRate = 80
+        
+WHERE VNum = 4712
+
+
+UPDATE Item
+
+    SET MaxElementRate = 120,
+        ElementRate = 80
+        
+WHERE VNum = 4713
+
+
+UPDATE Item
+
+    SET MaxElementRate = 120,
+        ElementRate = 80
+        
+WHERE VNum = 4714
+
+
+UPDATE Item
+
+    SET MaxElementRate = 120,
+        ElementRate = 80
+        
+WHERE VNum = 4715
+
+
+UPDATE Item
+
+    SET MaxElementRate = 120,
+        ElementRate = 80
+        
+WHERE VNum = 4716
