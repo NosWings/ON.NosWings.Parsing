a731787d705042b8be694eb996552c4842f29a0e
Elendan <arthur.despeaux@hotmail.fr>
2017-12-05 21:49:42 +0100

    Update remove monster script

--- various_db_scripts/RAIDS/REMOVE_RAID_MONSTERS.sql
+++ various_db_scripts/RAIDS/REMOVE_RAID_MONSTERS.sql
@@ -4,8 +4,11 @@ FROM
 WHERE
     [MapID] = '2511'
 OR [MapID] = '2512'
+OR [MapID] = '2514'
 OR [MapID] = '2521'
 OR [MapID] = '2522'
 OR [MapID] = '2537'
 OR [MapID] = '2538'
 OR [MapID] = '2589'
+OR [MapID] = '213'
+OR [MapID] = '220'
